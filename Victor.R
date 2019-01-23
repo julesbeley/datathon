57eb98efb562dd08faeca20f999951417664ad0a
Jules Beley <jules.beley@gmail.com>
2019-01-24 00:29:36 +0100

    attempting to zoom in

--- Victor.R
+++ Victor.R
@@ -42,50 +42,15 @@ p <- ggplot() +
   scale_fill_viridis(
     trans = "log",
     breaks = c(1, 5, 10, 20, 50, 100),
-    name = "Population density",
-    guide = guide_legend(
-      keyheight = unit(3, units = "mm"),
-      keywidth = unit(12, units = "mm"),
-      label.position = "right",
-      title.position = 'top',
-      nrow = 1
-    )
+    name = "Population density"
   ) +
   theme(
     text = element_text(color = "#22211d"),
     plot.background = element_rect(fill = "#f5f5f2", color = NA),
     panel.background = element_rect(fill = "#f5f5f2", color = NA),
-    legend.background = element_rect(fill = "#f5f5f2", color = NA),
-    
-    plot.title = element_text(
-      size = 22,
-      hjust = 0.01,
-      color = "#4e4d47",
-      margin = margin(
-        b = -0.1,
-        t = 0.4,
-        l = 2,
-        unit = "cm"
-      )
-    ),
-    plot.subtitle = element_text(
-      size = 17,
-      hjust = 0.01,
-      color = "#4e4d47",
-      margin = margin(
-        b = -0.1,
-        t = 0.43,
-        l = 2,
-        unit = "cm"
-      )
-    ),
-    plot.caption = element_text(
-      size = 12,
-      color = "#4e4d47",
-      margin = margin(b = 0.3, r = -99, unit = "cm")
-    ),
-    
-    legend.position = c(0.7, 0.09)
+    legend.background = element_rect(fill = "#f5f5f2", color = NA)
   ) +
-  coord_map()
+  coord_fixed(xlim = c(2.7,3.5),
+              ylim = c(50.5, 50.75),
+              ratio = 1.5)
 p
