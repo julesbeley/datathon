ff42508a10019585039bba3c6631036a99e9e508
Jules Beley <jules.beley@gmail.com>
2019-01-24 15:26:13 +0100

    Victor and Seyed

--- Script.R
+++ Script.R
@@ -32,39 +32,61 @@ edu3 <- edu2 %>%
 edu4 <- edu3[-c(101),]
 
 ##Graphing schools per individuals-----
+edu4$Catg <- as.character(edu4$Catg)
 
 edu4%>% group_by(Catg) %>% 
   summarise(meanKinder=mean(Kinder*1000/Pop)) %>% 
-  ggplot(aes(x= Catg, y = meanKinder, fill = meanKinder)) +
-  geom_col()+ expand_limits(x = 7) +  theme(axis.text.x=element_text(angle=90,hjust=1)) + theme(axis.text.y=element_text())+
-  ggtitle("Kindergardens per 1000 individulas per category") + xlab("Category of Urbanization") + ylab("Ratio") + guides(fill=FALSE)
+  ggplot(aes(x= Catg, y = meanKinder, fill = Catg)) +
+  geom_col()+ expand_limits(x = 7) + theme(axis.text.y=element_text())+
+  ggtitle("Preschools per 1000 individulas per category") + xlab("Category of Urbanization") + ylab("Ratio") + guides(fill=FALSE)+geom_text(aes(label = round(meanKinder, digits = 2)), position = "dodge")
+
 
 edu4%>% group_by(Catg) %>% 
   summarise(meanElementry=mean(Elementry*1000/Pop)) %>% 
-  ggplot(aes(x= Catg, y = meanElementry, fill = meanElementry)) +
-  geom_col()+ expand_limits(x = 7) +  theme(axis.text.x=element_text(angle=90,hjust=1)) + theme(axis.text.y=element_text())+
-  ggtitle("Elementry schools per 1000 individulas per category") + xlab("Category of Urbanization") + ylab("Ratio") + guides(fill=FALSE)
+  ggplot(aes(x= Catg, y = meanElementry, fill = Catg)) +
+  geom_col()+ expand_limits(x = 7)  + theme(axis.text.y=element_text())+
+  ggtitle("Elementry schools per 1000 individulas per category") + xlab("Category of Urbanization") + ylab("Ratio") + guides(fill=FALSE)+geom_text(aes(label = round(meanElementry, digits = 2)), position = "dodge")
 
 edu4%>% group_by(Catg) %>% 
   summarise(meanCollege=mean(College*1000/Pop)) %>% 
-  ggplot(aes(x= Catg, y = meanCollege, fill = meanCollege)) +
-  geom_col()+ expand_limits(x = 7) +  theme(axis.text.x=element_text(angle=90,hjust=1)) + theme(axis.text.y=element_text())+
-  ggtitle("Colleges per 1000 individulas per category") + xlab("Category of Urbanization") + ylab("Ratio") + guides(fill=FALSE)
+  ggplot(aes(x= Catg, y = meanCollege, fill = Catg)) +
+  geom_col()+ expand_limits(x = 7)  + theme(axis.text.y=element_text())+
+  ggtitle("Middle schools per 1000 individulas per category") + xlab("Category of Urbanization") + ylab("Ratio") + guides(fill=FALSE)+geom_text(aes(label = round(meanCollege, digits = 2)), position = "dodge")
 
 edu4%>% group_by(Catg) %>% 
   summarise(meanHigh=mean(High*1000/Pop)) %>% 
-  ggplot(aes(x= Catg, y = meanHigh, fill = meanHigh)) +
-  geom_col()+ expand_limits(x = 7) +  theme(axis.text.x=element_text(angle=90,hjust=1)) + theme(axis.text.y=element_text())+
-  ggtitle("High schools per 1000 individulas per category") + xlab("Category of Urbanization") + ylab("Ratio") + guides(fill=FALSE)
+  ggplot(aes(x= Catg, y = meanHigh, fill = Catg)) +
+  geom_col()+ expand_limits(x = 7)  + theme(axis.text.y=element_text())+
+  ggtitle("High schools per 1000 individulas per category") + xlab("Category of Urbanization") + ylab("Ratio") + guides(fill=FALSE)+geom_text(aes(label = round(meanHigh, digits = 2)), position = "dodge")
 
+edu4%>% group_by(Catg) %>% 
+  summarise(numb=n()) %>% 
+  ggplot(aes(x= Catg, y = numb, fill = Catg)) +
+  geom_col()+ expand_limits(x = 7) + theme(axis.text.y=element_text())+
+  ggtitle("Number of Departments in each category") + xlab("Category of Urbanization") + ylab("Number") + guides(fill=FALSE)+
+  geom_text(aes(label = round(numb)), position = "dodge")
+
+edu4%>% group_by(Catg) %>% 
+  summarise(meanRatio=mean(Ratio)) %>% 
+  ggplot(aes(x= Catg, y = meanRatio, fill = Catg)) +
+  geom_col()+ expand_limits(x = 7)  + theme(axis.text.y=element_text())+
+  ggtitle("Ratio of adults with no or partial diploma") + xlab("Category of Urbanization") + ylab("Ratio") + guides(fill=FALSE)+
+  geom_text(aes(label = round(meanRatio)), position = "dodge")
 
 ##Merging two Datasets-----
 Merged <- merge(edu4,TCRD_052,by="code")
 
 ##Graphing Public servants density in Categories
+Merged$Catg <- as.character(Merged$Catg)
 Merged%>% group_by(Catg) %>% 
   summarise(meanTPF100P=mean(TPubFoncper100P)) %>% 
-  ggplot(aes(x= Catg, y = meanTPF100P, fill = meanTPF100P)) +
-  geom_col()+ expand_limits(x = 7) +  theme(axis.text.x=element_text(angle=90,hjust=1)) + theme(axis.text.y=element_text())+
-  ggtitle("Public Servants per 100 individuals per category") + xlab("Category of Urbanization") + ylab("Ratio") + guides(fill=FALSE)
+  ggplot(aes(x= Catg, y = meanTPF100P, fill = Catg)) +
+  geom_col()+ expand_limits(x = 7) + theme(axis.text.y=element_text())+
+  ggtitle("Public Servants per 100 individuals per category") + xlab("Category of Urbanization") + ylab("Ratio") + guides(fill=FALSE)+geom_text(aes(label = round(meanTPF100P, digits = 2)), position = "dodge")
+
+Merged%>% group_by(Catg) %>% 
+  summarise(meanTPF100W=mean(TPubFoncper100W)) %>% 
+  ggplot(aes(x= Catg, y = meanTPF100W, fill = Catg)) +
+  geom_col()+ expand_limits(x = 7) + theme(axis.text.y=element_text())+
+  ggtitle("Public Servants per 100 workers per category") + xlab("Category of Urbanization") + ylab("Ratio") + guides(fill=FALSE)+geom_text(aes(label = round(meanTPF100W, digits = 2)), position = "dodge")
 
--- Victor.R
+++ Victor.R
@@ -1,21 +1,26 @@
-library(raster)
-library(sp)
-library(geojsonio)
-library(ggplot2)
-library(mapproj)
 library(tidyverse)
+library(ggplot2)
+library(tidyverse)
+library(geojsonio)
 library(broom)
-URL <-
-  "https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/communes.geojson"
-communes <- geojson_read(URL, what = "sp")
+library(viridis)
+
+communes <-
+  geojson_read("communes.geojson",
+               what = "sp")
+
 communes_fortified <- tidy(communes, region = "code")
+
 ggplot() +
   geom_polygon(data = communes_fortified , aes(x = long, y = lat, group = group)) +
   theme_void() +
   coord_map()
-donnée2 <- read.csv("./density.csv", header = T, sep = ",")
+
+donnée2 <- read.csv("density_pop3.csv", header = T, sep = ",")
+
 communes_fortified = communes_fortified %>%
   left_join(. , donnée2, by = c("id" = "COM"))
+
 ggplot() +
   geom_polygon(data = communes_fortified, aes(
     fill = P15_POP,
@@ -25,12 +30,27 @@ ggplot() +
   )) +
   theme_void() +
   coord_map()
-library(viridis)
+
+Ecoles <- read.csv("Ecoles.csv", header = T, sep = ";")
+
+Ecoles %>%
+  group_by(Code.commune) %>%
+  summarise(nbEcoles = n()) -> Ecoles
+
+communes_fortified = communes_fortified %>%
+  left_join(. , Ecoles, by = c("id" = "Code.commune"))
+
+communes_fortified %>%
+  filter(!is.na(nbEcoles)) -> communes_fortified
+
+communes_fortified %>%
+  mutate(P15_POP = P15_POP / nbEcoles) -> communes_fortified
+
 p <- ggplot() +
   geom_polygon(
     data = communes_fortified,
     aes(
-      fill = P15_POP,
+      fill = communes_fortified$P15_POP,
       x = long,
       y = lat,
       group = group
@@ -40,15 +60,26 @@ p <- ggplot() +
   ) +
   theme_void() +
   scale_fill_viridis(
-    trans = "log",
-    breaks = c(1, 5, 10, 20, 50, 100),
-    name = "Population density"
+    name ="Inhabitants School",
+    option = "inferno", direction = -1,
+    guide = guide_legend(
+      name = "Number of inhabitants",
+      keyheight = unit(3, units = "mm"),
+      keywidth = unit(8, units = "mm"),
+      label.position = "bottom",
+      nrow = 1
+    )
   ) +
   theme(
     text = element_text(color = "#22211d"),
     plot.background = element_rect(fill = "#f5f5f2", color = NA),
     panel.background = element_rect(fill = "#f5f5f2", color = NA),
-    legend.background = element_rect(fill = "#f5f5f2", color = NA)
-  ) +
-  coord_map()
+    legend.background = element_rect(fill = "#f5f5f2", color = NA),
+    plot.caption = element_text(
+      size = 12,
+      color = "#4e4d47",
+      margin = margin(b = 0.3, r = -99, unit = "cm")
+    ),
+    legend.position = c(0.7, 0.09)
+  ) + coord_map()
 p
